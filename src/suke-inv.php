b3265bb3d74fa44e156dce9cfed926e031175094
Ashwin <ashwinncj@gmail.com>
2020-07-16 16:45:58 +0530

    f/002: added register post type functions

--- src/suke-inv.php
+++ src/suke-inv.php
@@ -2,3 +2,6 @@
 /**
  * Plugin Name: Suke Invoice by Ashwin Inc(Ai)
  */
+
+require_once dirname( __FILE__ ) . '/class/main.php';
+register_activation_hook( __FILE__, array( 'SukeInv', 'activate' ) );
